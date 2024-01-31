808471d8b4b0b2c5ce62b798f5c43d81584b4771
An Byungwook <abw2619@icloud.com>
2024-01-31 14:10:05 +0900

    Initial Commit

---
+++ GestureTest/GestureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureTestApp.swift
+//  GestureTest
+//
+//  Created by 안병욱의 mac on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct GestureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
